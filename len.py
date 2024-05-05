b60ccd0afbfeea333a7ec04d37caf154c04ef2eb
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-05 23:45:43 +0530

    another commit

--- len.py
+++ len.py
@@ -1,3 +1,20 @@
 string1 = str(input("enetr the first string: "))
 length = len(string1)
 print("length is",length)
+
+
+# input_string = input("Enter a string: ")
+# length = 0
+# for char in input_string:
+#     length += 1
+# print("Length of the string:", length)
+
+
+# def string_length(input_string):
+#     length = 0
+#     for char in input_string:
+#         length += 1
+#     return length
+
+# input_string = input("Enter a string: ")
+# print("Length of the string:", string_length(input_string))
