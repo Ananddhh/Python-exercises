0301af0503460036bf9733f164d4ccba6b24d5a4
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-08 23:52:59 +0530

    another commit

--- longest.py
+++ longest.py
@@ -2,3 +2,11 @@ sentence = input("Enter a sentence: ")
 words = sentence.split()
 longest_word = max(words, key=len)
 print("Longest word:", longest_word)
+
+
+# import re
+
+# sentence = input("Enter a sentence: ")
+# words = re.findall(r'\b\w+\b', sentence)
+# longest_word = max(words, key=len)
+# print("Longest word:", longest_word)
