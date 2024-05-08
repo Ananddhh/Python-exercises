57af376735331d44c6c7ed0080826ea6bf48879e
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-08 23:52:30 +0530

    another commit

--- rev.py
+++ rev.py
@@ -2,3 +2,9 @@ sentence = input("Enter a sentence: ")
 words = sentence.split()
 reversed_sentence = ' '.join(reversed(words))
 print("Reversed sentence:", reversed_sentence)
+
+
+# sentence = input("Enter a sentence: ")
+# words = sentence.split()
+# reversed_sentence = ' '.join(words[::-1])
+# print("Reversed sentence:", reversed_sentence)
