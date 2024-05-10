0eed74151121cb8b87cc13c8a17d7e9de58a98c3
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-10 19:58:03 +0530

    another commit

--- rev.py
+++ rev.py
@@ -4,6 +4,14 @@ reversed_sentence = ' '.join(reversed(words))
 print("Reversed sentence:", reversed_sentence)
 
 
+
+
+# Reverse the list using slicing
+# char_list = ['a', 'b', 'c', 'd', 'e']
+# char_list = char_list[::-1]
+# print("Reversed list:", char_list)
+
+
 # sentence = input("Enter a sentence: ")
 # words = sentence.split()
 # reversed_sentence = ' '.join(words[::-1])
