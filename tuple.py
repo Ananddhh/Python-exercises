8b981c83a3a08ac5c3b0912f313b0728e65a531d
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-17 23:15:46 +0530

    another commit

---
+++ tuple.py
@@ -0,0 +1,11 @@
+# Finding Maximum and Minimum Values in a Tuple of Integers
+
+int_tuple = (5, 10, 3, 8, 15)
+
+# Find the maximum and minimum values
+max_value = max(int_tuple)
+min_value = min(int_tuple)
+
+# Print the results
+print("Maximum value:", max_value)
+print("Minimum value:", min_value)
