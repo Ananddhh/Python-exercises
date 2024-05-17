3c4bf74bc42b998972778c841cd2952a25f226f2
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-17 23:17:29 +0530

    another commit

---
+++ tuple2.py
@@ -0,0 +1,11 @@
+# Counting the Number of Occurrences of an Element in a Tuple
+my_tuple = (1, 2, 2, 3, 4, 2, 5, 2)
+
+# Choose an element to count
+element_to_count = 2
+
+# Count the number of occurrences of the element
+count = my_tuple.count(element_to_count)
+
+# Print the result
+print(f"Number of occurrences of {element_to_count}:", count)
