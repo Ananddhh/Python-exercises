0869291d9f24e010eda385040ccb291b52b61dc1
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-18 22:50:32 +0530

    another commit

---
+++ tupledict.py
@@ -0,0 +1,8 @@
+# Define a list of tuples
+list_of_tuples = [("key1", "value1"), ("key2", "value2"), ("key3", "value3")]
+
+# Convert the list of tuples into a dictionary
+dictionary = dict(list_of_tuples)
+
+# Print the resulting dictionary
+print("Dictionary:", dictionary)
