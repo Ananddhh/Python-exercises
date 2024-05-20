4a9c7f3deee1fc72467499264591560ab93c5135
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-20 23:00:41 +0530

    another commit

---
+++ diff2sets.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Find the difference between the sets (elements in set1 but not in set2)
+difference = set1.difference(set2)
+
+# Print the result
+print("Difference (set1 - set2):", difference)
