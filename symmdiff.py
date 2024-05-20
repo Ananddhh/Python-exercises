1c43077837e6cf11e213063eb20f5cd565718e71
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-20 23:01:09 +0530

    another commit

---
+++ symmdiff.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Find the symmetric difference (elements in either set1 or set2 but not both)
+symmetric_difference = set1.symmetric_difference(set2)
+
+# Print the result
+print("Symmetric difference:", symmetric_difference)
