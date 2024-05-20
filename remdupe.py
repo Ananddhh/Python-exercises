ef12bc4cb054003565b793b45e6826fcad041516
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-20 23:01:30 +0530

    another commit

---
+++ remdupe.py
@@ -0,0 +1,8 @@
+# Define a list with duplicates
+list_with_duplicates = [1, 2, 2, 3, 4, 4, 5]
+
+# Create a set from the list to remove duplicates
+unique_set = set(list_with_duplicates)
+
+# Print the resulting set
+print("Set with unique elements:", unique_set)
