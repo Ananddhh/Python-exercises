cce376d75e4cb1e1564b2e909cde17a94a7bd51b
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-21 17:24:51 +0530

    another commit

---
+++ sub.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {1, 2, 3}
+set2 = {1, 2, 3, 4, 5}
+
+# Check if set1 is a subset of set2
+is_subset = set1.issubset(set2)
+
+# Print the result
+print("Is set1 a subset of set2?", is_subset)
