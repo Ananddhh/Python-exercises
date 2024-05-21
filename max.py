0e39b0154c010e60b551dfd1784e645f872097c8
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-21 17:25:07 +0530

    another commit

---
+++ max.py
@@ -0,0 +1,10 @@
+# Define a set of numbers
+num_set = {10, 20, 5, 15, 25}
+
+# Find the maximum and minimum values in the set
+max_value = max(num_set)
+min_value = min(num_set)
+
+# Print the results
+print("Maximum value:", max_value)
+print("Minimum value:", min_value)
