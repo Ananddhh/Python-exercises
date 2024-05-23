ce0c088c4a60ebde7af61edb6b7a2d4ef4c42a8a
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-23 23:01:29 +0530

    another commit

---
+++ sumall.py
@@ -0,0 +1,8 @@
+# Define a set of numbers
+num_set = {10, 20, 5, 15, 25}
+
+# Find the sum of all elements in the set
+sum_of_elements = sum(num_set)
+
+# Print the result
+print("Sum of all elements in the set:", sum_of_elements)
