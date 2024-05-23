5aa228e832afd9a1f3e278e4a433fa648f197f11
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-23 23:01:04 +0530

    another commit

---
+++ union.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+# Update set1 with the union of itself and set2
+set1.update(set2)
+
+# Print the updated set
+print("Updated set1 with the union of set1 and set2:", set1)
