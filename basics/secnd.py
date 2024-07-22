d1c9ce6c356d66501d27cab9f41c09fd0f67b4c8
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-22 20:36:18 +0530

    another commit

--- basics/secnd.py
+++ basics/secnd.py
@@ -7,3 +7,15 @@ y = "Hello, World!"
 
 #This is a comment.
 print("Hello, World!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
