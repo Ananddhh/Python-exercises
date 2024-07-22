1f8aebae3a8de5b08776a55b398fde728c3aa573
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-22 20:40:58 +0530

    another commit

--- basics/thrd.py
+++ basics/thrd.py
@@ -59,3 +59,28 @@ x, y, z = fruits
 print(x)
 print(y)
 print(z)
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
