7a96930289d79931d036f4109c42d95f308c922f
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-22 20:38:31 +0530

    another commit

--- basics/thrd.py
+++ basics/thrd.py
@@ -11,3 +11,27 @@ print(x)
 x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
 z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+# Legal variable names
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Illegal variable names
+
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
