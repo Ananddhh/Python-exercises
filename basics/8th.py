bdbc464a29a22f590496b974beb4d1f10a243b9f
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-25 12:52:06 +0530

    another commit

--- basics/8th.py
+++ basics/8th.py
@@ -98,3 +98,18 @@ def myFunction() :
   return True
 
 print(myFunction())
+
+# Print "YES!" if the function returns True, otherwise print "NO!":
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+# Check if an object is an integer or not:
+
+x = 200
+print(isinstance(x, int))
