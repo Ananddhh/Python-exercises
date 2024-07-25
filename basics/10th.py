32f78ead1a14b1223550089bfc1105f884afcd35
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-25 12:54:12 +0530

    another commit

---
+++ basics/10th.py
@@ -0,0 +1,10 @@
+# This example returns the items from "orange" (-4) to, but NOT including "mango" (-1):
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+# Check if "apple" is present in the list:
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
--- basics/9th.py
+++ basics/9th.py
@@ -50,3 +50,9 @@ print(thislist[2:5])
 
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[:4])
+
+# This example returns the items from "cherry" to the end:
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
