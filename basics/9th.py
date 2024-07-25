093d91205df4b8a9ffee34b609ee17c85fb6efa4
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-25 12:52:51 +0530

    another commit

--- basics/9th.py
+++ basics/9th.py
@@ -35,3 +35,8 @@ print(thislist)
 
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
+
+# Print the last item of the list:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
