a799594556debe98c9df17fd429981fd556af0b9
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-25 12:53:18 +0530

    another commit

--- basics/9th.py
+++ basics/9th.py
@@ -40,3 +40,13 @@ print(thislist[1])
 
 thislist = ["apple", "banana", "cherry"]
 print(thislist[-1])
+
+# Return the third, fourth, and fifth item:
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+# This example returns the items from the beginning to, but NOT including, "kiwi":
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
