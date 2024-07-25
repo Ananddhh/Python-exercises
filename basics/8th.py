982057e65c4780a47442ba36c290923c6d4ed73b
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-25 12:47:16 +0530

    another commit

---
+++ basics/8th.py
@@ -0,0 +1,33 @@
+# String Concatenation
+# Merge variable a with variable b into variable c:
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+# To add a space between them, add a " ":
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
+age = 36
+txt = "My name is John, I am " + age
+print(txt)
+
+
+# Create an f-string:
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+
+# Add a placeholder for the price variable:
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
