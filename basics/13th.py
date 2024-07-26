793e4cd0d6b39ef393ad930aa1b6bebd6dfdb191
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-26 20:14:03 +0530

    another commit

--- basics/13th.py
+++ basics/13th.py
@@ -19,6 +19,22 @@ print(newlist)
 
 newlist = [x for x in fruits if x != "apple"]
 
-With no if statement:
+# With no if statement:
 
 newlist = [x for x in fruits]
+
+# You can use the range() function to create an iterable:
+
+newlist = [x for x in range(10)]
+
+# Accept only numbers lower than 5:
+
+newlist = [x for x in range(10) if x < 5]
+
+# Set the values in the new list to upper case:
+
+newlist = [x.upper() for x in fruits]
+
+# Set all values in the new list to 'hello':
+
+newlist = ['hello' for x in fruits]
