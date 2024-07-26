9d22d334fc58ff9e3c9f5a1c986801cef38bfe6e
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-26 20:13:07 +0530

    another commit

--- basics/13th.py
+++ basics/13th.py
@@ -12,3 +12,13 @@ fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = [x for x in fruits if "a" in x]
 
 print(newlist)
+
+# newlist = [expression for item in iterable if condition == True]
+
+# Only accept items that are not "apple":
+
+newlist = [x for x in fruits if x != "apple"]
+
+With no if statement:
+
+newlist = [x for x in fruits]
