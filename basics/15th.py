afa306a203aa786c111d85e2149255c526067cf4
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-27 22:51:19 +0530

    another commit

--- basics/15th.py
+++ basics/15th.py
@@ -18,3 +18,19 @@ print(thistuple[2:5])
 
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[:4])
+
+# This example returns the items from "cherry" and to the end:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+# This example returns the items from index -4 (included) to index -1 (excluded)
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+# Check if "apple" is present in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
