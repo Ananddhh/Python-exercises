e8a715f72453b1ba98b957dba8b3e01a787f4b5e
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-27 22:49:21 +0530

    another commit

--- basics/14th.py
+++ basics/14th.py
@@ -51,3 +51,27 @@ print(thistuple)
 
 thistuple = ("apple", "banana", "cherry")
 print(len(thistuple))
+
+# One item tuple, remember the comma:
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+# String, int and boolean data types:
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+# A tuple with strings, integers and boolean values:
+
+tuple1 = ("abc", 34, True, 40, "male")
+
+# What is the data type of a tuple?
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
