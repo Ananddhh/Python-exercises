40cda56fd776770b7ea66eea917f46ac3f6eceb5
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-28 21:17:52 +0530

    another commit

--- basics/17th.py
+++ basics/17th.py
@@ -5,3 +5,33 @@ tuple2 = (1, 2, 3)
 
 tuple3 = tuple1 + tuple2
 print(tuple3)
+
+# Multiply the fruits tuple by 2:
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+
+# Create a Set:
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+# Duplicate values will be ignored:
+
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+
+# True and 1 is considered the same value:
+
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+
+# False and 0 is considered the same value:
+
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
