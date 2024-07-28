dd3d842a8fb50248bb546870ec5db3e26bcb4bdd
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-28 21:20:14 +0530

    another commit

--- basics/18th.py
+++ basics/18th.py
@@ -17,3 +17,30 @@ print("banana" in thisset)
 thisset = {"apple", "banana", "cherry"}
 
 print("banana" not in thisset)
+
+# Add an item to a set, using the add() method:
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+# Add elements from tropical into thisset:
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+
+# Add elements of a list to at set:
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
