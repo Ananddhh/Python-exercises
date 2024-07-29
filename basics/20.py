1166c38f8f6658b278e4ead85708f83b63a13991
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-29 20:13:39 +0530

    another commit

--- basics/20.py
+++ basics/20.py
@@ -30,3 +30,46 @@ set4 = {"apple", "bananas", "cherry"}
 
 myset = set1.union(set2, set3, set4)
 print(myset)
+
+# Use | to join two sets:
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1 | set2 | set3 |set4
+print(myset)
+
+# Join a set with a tuple:
+
+x = {"a", "b", "c"}
+y = (1, 2, 3)
+
+z = x.union(y)
+print(z)
+
+# The update() method inserts the items in set2 into set1:
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)
+
+
+# Join set1 and set2, but keep only the duplicates:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.intersection(set2)
+print(set3)
+
+# Use & to join two sets:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 & set2
+print(set3)
