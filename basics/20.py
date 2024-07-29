fd1b7fcd4c68bef6e9bf58acc6d0e7da763a0c4a
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-29 20:14:32 +0530

    another commit

--- basics/20.py
+++ basics/20.py
@@ -108,3 +108,12 @@ set2 = {"google", "microsoft", "apple"}
 
 set3 = set1 - set2
 print(set3)
+
+# Use the difference_update() method to keep the items that are not present in both sets:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.difference_update(set2)
+
+print(set1)
