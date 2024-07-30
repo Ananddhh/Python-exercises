1c80f8ae16183b87d91cd22541bd33f81e63d13d
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-30 11:44:22 +0530

    another commit

--- basics/21.py
+++ basics/21.py
@@ -150,3 +150,29 @@ print(x) #before the change
 car["year"] = 2020
 
 print(x) #after the change
+
+# Add a new item to the original dictionary, and see that the items list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
+
+# Check if "model" is present in the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
