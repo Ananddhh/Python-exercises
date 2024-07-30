8620b92a59ce612b4fc6c8718bbaea176518611c
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-30 11:44:00 +0530

    another commit

--- basics/21.py
+++ basics/21.py
@@ -114,3 +114,39 @@ print(x) #before the change
 car["year"] = 2020
 
 print(x) #after the change
+
+# Add a new item to the original dictionary, and see that the values list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
+
+# Get a list of the key:value pairs
+
+x = thisdict.items()
+
+# Make a change in the original dictionary, and see that the items list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+
+print(x) #before the change
+
+car["year"] = 2020
+
+print(x) #after the change
