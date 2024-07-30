ab4c64d94819dc53e6741c1815c2afbb6d7e5cbb
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-30 11:56:22 +0530

    another commit

--- basics/24.py
+++ basics/24.py
@@ -21,3 +21,8 @@ for x in thisdict.values():
 
 for x in thisdict.keys():
   print(x)
+
+# Loop through both keys and values, by using the items() method:
+
+for x, y in thisdict.items():
+  print(x, y)
