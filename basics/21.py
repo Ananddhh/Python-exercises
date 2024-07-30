96396b55c8820e9c3985b04312890fa551696402
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-30 11:43:28 +0530

    another commit

--- basics/21.py
+++ basics/21.py
@@ -78,3 +78,39 @@ x = thisdict.get("model")
 # Get a list of the keys:
 
 x = thisdict.keys()
+
+# Add a new item to the original dictionary, and see that the keys list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
+
+# Get a list of the values:
+
+x = thisdict.values()
+
+# Make a change in the original dictionary, and see that the values list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+
+print(x) #before the change
+
+car["year"] = 2020
+
+print(x) #after the change
