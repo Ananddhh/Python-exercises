46db39c8ea15928773569d58d7a3bd8301ade25a
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-31 19:45:24 +0530

    another commit

--- basics/26.py
+++ basics/26.py
@@ -45,3 +45,20 @@ print("A") if a > b else print("B")
 a = 330
 b = 330
 print("A") if a > b else print("=") if a == b else print("B")
+
+
+# Test if a is greater than b, AND if c is greater than a:
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+# Test if a is greater than b, OR if a is greater than c:
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
