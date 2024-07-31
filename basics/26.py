f49dc46c918de9aa51c7dbb2e5c429bdeb58a3e1
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-31 19:44:53 +0530

    another commit

--- basics/26.py
+++ basics/26.py
@@ -39,3 +39,9 @@ if a > b: print("a is greater than b")
 a = 2
 b = 330
 print("A") if a > b else print("B")
+
+# One line if else statement, with 3 conditions:
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
