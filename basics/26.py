d20f02766111ef3aeae5cfc9f2fc833f10cda962
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-07-31 19:45:46 +0530

    another commit

--- basics/26.py
+++ basics/26.py
@@ -62,3 +62,11 @@ b = 33
 c = 500
 if a > b or a > c:
   print("At least one of the conditions is True")
+
+
+# Test if a is NOT greater than b:
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
