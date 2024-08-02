b5c1e8a10f4f02bfdf70b480db142bf8f4704e11
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-02 18:27:41 +0530

    another commit

--- basics/27.py
+++ basics/27.py
@@ -49,3 +49,12 @@ while i < 6:
   if i == 3:
     continue
   print(i)
+
+# Print a message once the condition is false:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
