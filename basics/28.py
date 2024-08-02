facda5a22bd67b8695db6ce210a1c417600fe6a6
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-02 18:28:40 +0530

    another commit

--- basics/28.py
+++ basics/28.py
@@ -3,3 +3,16 @@
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
+
+# Loop through the letters in the word "banana":
+
+for x in "banana":
+  print(x)
+
+# Exit the loop when x is "banana":
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
