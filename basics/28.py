cb4943a9fadcc856fd263f52f2a9653b5aa2bfa9
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-04 23:12:43 +0530

    another commit

--- basics/28.py
+++ basics/28.py
@@ -55,3 +55,11 @@ for x in range(6):
   print(x)
 else:
   print("Finally finished!")
+
+# Break the loop when x is 3, and see what happens with the else block:
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
