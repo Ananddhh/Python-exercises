84271093ef2e0651df1eef01e63edbeb55785e27
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-05 12:24:35 +0530

    another commit

--- basics/29.py
+++ basics/29.py
@@ -34,3 +34,11 @@ def my_function(fname, lname):
   print(fname + " " + lname)
 
 my_function("Emil")
+
+
+# If the number of arguments is unknown, add a * before the parameter name:
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
