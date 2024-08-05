553c8e97d1309f3d592edd90368b75725e396c10
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-05 12:25:13 +0530

    another commit

--- basics/29.py
+++ basics/29.py
@@ -42,3 +42,9 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+
+# Example
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
