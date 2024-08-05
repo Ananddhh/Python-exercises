2a4099d152d6ee2430b79f453637b66bd8c5460f
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-05 12:21:49 +0530

    another commit

---
+++ basics/29.py
@@ -0,0 +1,11 @@
+# In Python a function is defined using the def keyword:
+
+
+def my_function():
+  print("Hello from a function")
+
+
+def my_function():
+  print("Hello from a function")
+# To call a function, use the function name followed by parenthesis:
+my_function()
