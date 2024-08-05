cc3fc88413ee8346747df87da4776c1b7396efb6
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-05 12:25:41 +0530

    another commit

--- basics/29.py
+++ basics/29.py
@@ -48,3 +48,12 @@ def my_function(child3, child2, child1):
   print("The youngest child is " + child3)
 
 my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+# Example
+# If the number of keyword arguments is unknown, add a double ** before the parameter name:
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
