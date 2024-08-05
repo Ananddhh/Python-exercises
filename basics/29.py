c6517b6993e7fadbcfdcffc4af88a7d68d07c472
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-05 12:26:59 +0530

    another commit

--- basics/29.py
+++ basics/29.py
@@ -80,3 +80,13 @@ def my_function(food):
 fruits = ["apple", "banana", "cherry"]
 
 my_function(fruits)
+
+# To let a function return a value, use the return statement:
+
+# Example
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
