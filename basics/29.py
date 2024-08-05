31120eaea02e5058c9a3b06c245d33e555218d60
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-05 12:24:23 +0530

    another commit

--- basics/29.py
+++ basics/29.py
@@ -19,3 +19,18 @@ def my_function(fname):
 my_function("Emil")
 my_function("Tobias")
 my_function("Linus")
+
+
+# This function expects 2 arguments, and gets 2 arguments:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+# This function expects 2 arguments, but gets only 1:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil")
