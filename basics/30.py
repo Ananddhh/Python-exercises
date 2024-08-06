6540a7ed852e140ef4e30958d9efbe168d8297d1
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-06 19:03:43 +0530

    another commit

--- basics/30.py
+++ basics/30.py
@@ -22,3 +22,10 @@ def my_function(*, x):
   print(x)
 
 my_function(x = 3)
+
+# any arguments before the / , are positional-only, and NY rgument after the *, are keyword-only:
+
+def my_function(a, b, /, *, c, d):
+  print(a + b + c + d)
+
+my_function(5, 6, c = 7, d = 8)
