eb1c14bebdce516fac706b689f2a6f2e45987740
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-06 19:06:53 +0530

    another commit

--- basics/30.py
+++ basics/30.py
@@ -29,3 +29,16 @@ def my_function(a, b, /, *, c, d):
   print(a + b + c + d)
 
 my_function(5, 6, c = 7, d = 8)
+
+# recursion example:
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print (result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion example Results")
+tri_recursion(6)
