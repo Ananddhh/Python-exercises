5caed7362c1c725757435ac95de46273ba1998e4
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-07 14:07:07 +0530

    another commit

---
+++ basics/array.py
@@ -0,0 +1,10 @@
+# create an array containing car names: 
+
+cars = ["ford", "volvo", "BMW"]
+
+x = cars[0]
+cars[0] = "toyota"
+
+# return the number of elements in the cars array:
+
+x = len(cars)
--- basics/lambda.py
+++ basics/lambda.py
@@ -47,5 +47,6 @@ mydoubler = myfunc(2)
 mytripler = myfunc(3)
 
 print(mydoubler(11))
-print(mytripler(11)
-      )
+print(mytripler(11))
+
+
