7e51a1d059524d9e1a454e0efe3b547fe453fb9a
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-08-07 14:01:31 +0530

    another commit

--- basics/lambda.py
+++ basics/lambda.py
@@ -37,3 +37,15 @@ def myfunc(n):
 mytripler = myfunc(3)
 
 print(mytripler(11))
+
+# or, use the same function definition to make a both functions, in the same program:
+
+def myfunc(n):
+    return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11)
+      )
